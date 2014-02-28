a5c2dea7efebb8e0ad6dbe4319e3f55812c63ea4
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2014-02-28 16:25:36 -0300

    Improved travis output

--- bin/travis/script.sh
+++ bin/travis/script.sh
@@ -5,11 +5,13 @@ phpunit --configuration=tests/phpunit.xml
 php --version | grep 5.5 > /dev/null
 
 if (( $? == 0 )); then
-  echo "Installing PHP Code Sniffer"
-
-  pyrus install pear/PHP_CodeSniffer
-  phpenv rehash
-
-  echo "Checking code standard"
+  echo -en "\n\nChecking code standard..."
   phpcs --standard=Zend lib
+
+  if (( $? == 0 )); then
+    echo -en '\E[32m'"\033[1mPASSED!\033[0m\n\n" # Green
+  else
+    echo   -en '\E[31;47m'"\033[1mFAILED!\033[0m\n\n"   # Red
+    exit 1
+  fi
 fi
